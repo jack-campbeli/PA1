67a36873e3f4640bb2352636a717533356a71fef
Jack Campbell <jackwcampbell11@gmail.com>
2023-02-22 16:20:34 -0500

    Pictures to Photo.

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def register():
 
 @app.route("/photos", methods=['GET'])
 @flask_login.login_required
-def pictures():
+def Photo():
     uid = getUserIdFromEmail(flask_login.current_user.id)
     return render_template('hello.html', message='These are your photos', photos=getUsersPhotos(uid), base64=base64)
 
@@ -179,7 +179,7 @@ def register_user():
 def getUsersPhotos(uid):
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT imgdata, picture_id, caption FROM Pictures WHERE user_id = '{0}'".format(uid))
+        "SELECT imgdata, photo_id, caption FROM Photo WHERE user_id = '{0}'".format(uid))
     # return a list of tuples, [(imgdata, pid, caption), ...]
     return cursor.fetchall()
 
@@ -267,10 +267,10 @@ def upload_file():
         uid = getUserIdFromEmail(flask_login.current_user.id)
         imgfile = request.files['photo']
         caption = request.form.get('caption')
-        photo_data = imgfile.read()
+        imgdata = imgfile.read()
         cursor = conn.cursor()
         cursor.execute(
-            '''INSERT INTO Pictures (imgdata, user_id, caption) VALUES (%s, %s, %s)''', (photo_data, uid, caption))
+            '''INSERT INTO Photo (imgdata, user_id, caption) VALUES (%s, %s, %s)''', (imgdata, uid, caption))
         conn.commit()
         return render_template('hello.html', name=flask_login.current_user.id, message='Photo uploaded!', photos=getUsersPhotos(uid), base64=base64)
     # The method is GET so we return a HTML form to upload the a photo.
@@ -279,22 +279,20 @@ def upload_file():
 # end photo uploading code
 
 # start photo deleting code
-
-
-@app.route('/delete', methods=['GET', 'POST'])
+@app.route('/delete', methods=['GET','POST'])
 @flask_login.login_required
 def delete_photo():
-    if request.method == 'POST':
-        uid = getUserIdFromEmail(flask_login.current_user.id)
-        picture_id = request.form.get('picture_id')
-        cursor = conn.cursor()
-        cursor.execute(
-            "DELETE FROM Pictures WHERE picture_id = '{0}'".format(picture_id))
-        conn.commit()
-        return render_template('hello.html', name=flask_login.current_user.id, message='Photo deleted!', photos=getUsersPhotos(uid), base64=base64)
+    if request.method == 'GET':
+        return render_template('hello.html', delete=True, name=flask_login.current_user.id, message='Delete a photo!', photos=getUsersPhotos(uid), base64=base64)
     else:
         uid = getUserIdFromEmail(flask_login.current_user.id)
-        return render_template('hello.html', message="Delete photos here!", delete=True, photos=getUsersPhotos(uid), base64=base64)
+        photo_id = request.form.get('photo_id')
+        cursor = conn.cursor()
+        cursor.execute(
+            "DELETE FROM Photo WHERE photo_id = '{0}'".format(photo_id))
+        conn.commit()
+        return render_template('hello.html', delete=True, name=flask_login.current_user.id, message='Photo deleted!', photos=getUsersPhotos(uid), base64=base64)
+
 
 # default page
 
